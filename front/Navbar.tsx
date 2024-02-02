297a4c09ed0b5a63c64fe1cc1c04296ac4775226
Ponpan Bunkham <151675683+Bponpan@users.noreply.github.com>
2024-02-02 22:18:47 +0700

    Create Navbar.tsx

---
+++ front/Navbar.tsx
@@ -0,0 +1,45 @@
+<nav>
+      <div className="bg-[#70C174] bg-cover p-8 ">
+        <div className="text-white text-5xl xl:font-serif font-bold">
+          <Link href="/page">DEMISTORY SYSTEM</Link></div>
+      </div>
+
+      <div className="bg-green-200 flex grid-cols-2 gap-4 place-items-stretch px-10">
+        <div className="flex justify-between gap-px px-8">
+          <Image src={pictu} alt="" width="85" ></Image>
+        </div>
+
+        <div className="px-10">
+          <div className="text-green-50 text-base font-semibold my-10 ">
+            <Link href="/report_repair">Report repair</Link>
+            </div>
+        </div>
+
+        <div className="px-10 ">
+          <div className="text-green-50 text-base font-semibold relative my-10">
+          <Link href="/report_list">Report List</Link>
+          </div>
+        </div>
+
+        <div className="px-8 flex ml-auto items-center">
+          <div className="px-5 flex justify-end ">
+            <div className="relative my-8 ">
+            <Image src={balls} alt="" width="30"></Image>
+            </div>
+          </div>
+
+          <div className="px-5 flex justify-end ">
+            <div className="text-[#525560] font-semibold relative my-8">
+            <div className="text-right text-sm">WORAMET Y</div>
+            <div className="text-right text-sm">User</div>
+            </div>
+          </div>
+
+          <div className="flex justify-self-center ">
+            <div className="w-14 h-14 bg-[#525560] rounded-[10px] my-6 ">
+            </div>
+          </div>
+        </div>
+        
+      </div>
+      </nav>
